2883bff8890ab466743d79427e4d83196dd93c9d
Gio <gioskate00@gmail.com>
2016-02-27 22:25:04 +0100

    Theme fixing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,86 +2,97 @@ var options = { 'host': 'localhost', 'port': 6800, 'secure': false }
 var aria2 = new Aria2(options)
 var isOpen = false
 aria2.open(function() {
-isOpen = true
-
-Vue.config.debug = true;
-var v = new Vue({
-    el: '#body',
-    data: {
-        urlfield: '',
-        toggle: false,
-        connected: false,
-        active: [],
-        waiting: [],
-        stopped: []
-    },
-    ready: function() {
-        setInterval(this.initAria, 1000);
-        this.setupNotifications();
-        var self = this;
-        aria2.getGlobalOption(function(err,res){
-            self.$set('options', res)
-        });
-    },
-    computed: {
-        aria2: function() {
-            return aria2
+    Vue.config.debug = true;
+    var v = new Vue({
+        el: '#body',
+        data: {
+            urlfield: '',
+            ariaOpt: {},
+            toggle: false,
+            connected: false,
+            options:{},
+            active: [],
+            waiting: [],
+            stopped: []
         },
-        all: function() {
-            return this.stopped.concat(this.waiting).concat(this.active)
-        }
-    },
-    methods: {
-        setupNotifications: function() {
-            notify.requestPermission(function() {
-                var permissionLevel = notify.permissionLevel();
-                var permissionsGranted = (permissionLevel === notify.PERMISSION_GRANTED);
-            });
+        ready: function() {
+            setInterval(this.initAria, 1000);
+            this.ariaOpt = options
+            this.setupNotifications();
             var self = this;
-            aria2.onDownloadComplete = function(gid) {
-                self.notify(gid["gid"], 'Download Complete')
-            };
-            aria2.onDownloadStart = function(gid) {
-                self.notify(gid["gid"], 'Download Started')
+            aria2.getGlobalOption(function(err, res) {
+                self.options = res
+            });
+        },
+        computed: {
+            aria2: function() {
+                return aria2
+            },
+            all: function() {
+                return this.stopped.concat(this.waiting).concat(this.active)
             }
         },
-        notify: function(gid, message) {
-            aria2.getFiles(gid, function(err, res) {
-                this.notifyError(err)
-                notify.createNotification((res[0].path.split('/'))[res[0].path.split('/').length - 1], {
-                    body: message,
-                    icon: "images/icon.png"
+        methods: {
+            relaodAria: function() {
+                aria2 = new Aria2(this.ariaOpt)
+                aria2.open(function() {});
+                this.toggle = ! this.toggle
+            },
+            setupNotifications: function() {
+                notify.requestPermission(function() {
+                    var permissionLevel = notify.permissionLevel();
+                    var permissionsGranted = (permissionLevel === notify.PERMISSION_GRANTED);
                 });
-            });
-        },
-        notifyError: function(err) {
-            notify.createNotification("Errore", {
-                body: err["message"],
-                icon: "images/icon.png"
-            });
-        },
-        optCallback: function(err,res){
-            $('#alertspace').append('<div id="alert" class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>'+res+'</span></div>')
-            setTimeout( function(){ 
-              $('#alert').remove();
-            }  , 4000 );
-        },
-        callback: function(err, res) {
-            if (err) {
-                this.notifyError(err)
-            }
-            console.log(res)
-        },
-        byteCount: function(bytes, unit) {
-            if (bytes < (unit = unit || 1000))
-                return bytes + " B";
-            var exp = Math.floor(Math.log(bytes) / Math.log(unit));
-            var pre = ' ' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit === 1000 ? "" : "i") + 'B';
-            return (bytes / Math.pow(unit, exp)).toFixed(1) + pre;
-        },
-        initAria: function() {
-            var self = this;
-            if (isOpen) {
+                var self = this;
+                aria2.onDownloadComplete = function(gid) {
+                    self.notify(gid["gid"], 'Download Complete')
+                };
+                aria2.onDownloadStart = function(gid) {
+                    self.notify(gid["gid"], 'Download Started')
+                }
+            },
+            notify: function(gid, message) {
+                aria2.getFiles(gid, function(err, res) {
+                    if (undefined != err) {
+                        this.notifyError(err)
+                    }
+                    notify.createNotification((res[0].path.split('/'))[res[0].path.split('/').length - 1], {
+                        body: message,
+                        icon: "images/icon.png"
+                    });
+                });
+            },
+            notifyError: function(err) {
+                if (undefined != err) {
+                    notify.createNotification("Errore", {
+                        body: err["message"],
+                        icon: "images/icon.png"
+                    });
+                }
+            },
+            optCallback: function(err, res) {
+                $('#alertspace').append('<div id="alert" class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>' + res + '</span></div>')
+                setTimeout(function() {
+                    $('#alert').remove();
+                }, 4000);
+                this.toggle = ! this.toggle
+            },
+            callback: function(err, res) {
+                if (err) {
+                    this.notifyError(err)
+                }
+                console.log(res)
+            },
+            byteCount: function(bytes, unit) {
+                if (bytes < (unit = unit || 1000))
+                    return bytes + " B";
+                var exp = Math.floor(Math.log(bytes) / Math.log(unit));
+                var pre = ' ' + (unit === 1000 ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (unit === 1000 ? "" : "i") + 'B';
+                return (bytes / Math.pow(unit, exp)).toFixed(1) + pre;
+            },
+            initAria: function() {
+                var self = this;
+
                 aria2.tellActive(function(err, res) {
                     if (err) {
                         self.notifyError(err)
@@ -111,6 +122,5 @@ var v = new Vue({
                 });
             }
         }
-    }
-})
+    })
 });
